5f6a499337996cba4a6662b769aa78cdba099037
Bereczki Sándor Máté <sandor.bereczki@globalvet.com>
2025-10-05 17:15:46 +0200

    Fix style import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import './styles.css';
 import type { ReactNode } from 'react';
 
 const geistSans = Geist({

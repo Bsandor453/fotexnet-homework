7de1285671d410a160f8ccca2bd9c56bb2e4be9e
Bereczki Sándor Máté <sandor.bereczki@globalvet.com>
2025-10-05 17:38:20 +0200

    Add a simple page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
+import React from 'react';
+import { Layout } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+
 export default function Home() {
+  const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 48,
+    lineHeight: '64px',
+    backgroundColor: '#4096ff',
+  };
+
+  const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#0958d9',
+  };
+
+  const footerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#4096ff',
+  };
+
+  const layoutStyle = {
+    width: '100%',
+    height: '100%',
+  };
+
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <p>Placeholder content</p>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
     </div>
   );
 }

823a6f871ad40a3def329c6488fbee05043cb1f7
Bereczki Sándor Máté <sandor.bereczki@globalvet.com>
2025-10-05 19:06:09 +0200

    Add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useCallback, useEffect, useState } from 'react';
-import { Button, Col, Input, Layout, Row } from 'antd';
+import { Button, Col, Input, Layout, Pagination, Row } from 'antd';
 import { Content, Footer, Header } from 'antd/es/layout/layout';
 import { fetchArtists } from '@/api/artistsApi';
 import { GetArtistsRequest } from '@/interfaces/request/GetArtistsRequest';
@@ -44,25 +44,51 @@ const layoutStyle = {
   height: '100%',
 };
 
+const paginationStyle = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '2rem',
+};
+
 export default function Home() {
+  // Artists data
   const [artists, setArtists] = useState<Artist[]>([]);
+
+  // Filters
   const [search, setSearch] = useState<string>('');
   const [startsWithLetter, _setStartsWithLetter] = useState<string>('');
 
+  // Pagination
+  const [page, setPage] = useState<number>(1);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [perPage, setPerPage] = useState<number>(50);
+
   const getArtists = useCallback(
-    async (params?: { search?: string; startsWithLetter?: string }) => {
+    async (params?: {
+      search?: string;
+      startsWithLetter?: string;
+      page?: number;
+      perPage?: number;
+    }) => {
       const config: GetArtistsRequest = {
         artistType: 'performer',
         includeImage: true,
-        page: 1,
-        per_page: 20,
+        ...(params?.page ? { page: params.page } : { page: 1 }),
+        ...(params?.perPage && { per_page: params.perPage }),
         ...(params?.search && { search: params.search }),
         ...(params?.startsWithLetter && { startsWithLetter: params.startsWithLetter }),
       };
 
       try {
         const response = await fetchArtists(config);
+
+        // Arist data
         setArtists(response.data);
+
+        // Pagination data
+        const { pagination } = response;
+        setPage(pagination.current_page);
+        setTotalItems(pagination.total_items);
       } catch (error) {
         console.error('Error setting artist:', error);
       }
@@ -74,12 +100,21 @@ export default function Home() {
     void getArtists();
   }, [getArtists]);
 
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
 
-  const onSearchButtonClick = () => {
-    void getArtists({ search, startsWithLetter });
+  const handleSearchButtonClick = () => {
+    void getArtists({ search, startsWithLetter, page, perPage });
+  };
+
+  const handlePageChange = (newPage: number, _newPageSize: number) => {
+    setPage(newPage);
+    void getArtists({ search, startsWithLetter, page: newPage, perPage });
+  };
+
+  const handleShowSizeChange = (_current: number, size: number) => {
+    setPerPage(size);
   };
 
   return (
@@ -89,14 +124,14 @@ export default function Home() {
           <section className="flex flex-row" id="filters">
             <div className="w-64">
               <Input
-                onChange={onInputChange}
+                onChange={handleInputChange}
                 size="large"
                 placeholder={Strings.searchInputPlaceholder}
                 prefix={<UserOutlined />}
               />
             </div>
             <div className="w-32">
-              <Button onClick={onSearchButtonClick}>{Strings.searchButtonText}</Button>
+              <Button onClick={handleSearchButtonClick}>{Strings.searchButtonText}</Button>
             </div>
           </section>
         </Header>
@@ -118,6 +153,17 @@ export default function Home() {
                 </Col>
               ))}
             </Row>
+            <div hidden={artists.length === 0}>
+              <Pagination
+                style={paginationStyle}
+                current={page}
+                defaultCurrent={1}
+                total={totalItems}
+                pageSize={perPage}
+                onChange={handlePageChange}
+                onShowSizeChange={handleShowSizeChange}
+              />
+            </div>
           </>
         </Content>
         <Footer style={footerStyle}>Footer</Footer>
